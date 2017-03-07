ddd860b318972768871889fb26a61bcf46e66ea6
Andrew Chilton <andychilton@gmail.com>
2017-03-07 15:01:03 +1300

    Serve the homepage with no errors

--- Makefile
+++ Makefile
@@ -7,6 +7,10 @@ vendor:
 fmt:
 	find src/ -name '*.go' -exec go fmt {} ';'
 
+assets:
+	curl -X POST -s --data-urlencode 'input@static/s/js/ready.js' https://javascript-minifier.com/raw > static/s/js/ready.min.js
+	curl -X POST -s --data-urlencode 'input@static/s/css/style.css' https://cssminifier.com/raw > static/s/css/style.min.css
+
 build: fmt
 	gb build all
 
--- src/cmd/cssminifier/cssminifier.go
+++ src/cmd/cssminifier/cssminifier.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gomiddleware/mux"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// setup
+	baseUrl := os.Getenv("CSSMINIFIER_BASE_URL")
+	port := os.Getenv("CSSMINIFIER_PORT")
+	if port == "" {
+		log.Fatal("Specify a port to listen on in the environment variable 'CSSMINIFIER_PORT'")
+	}
+	googleAnalytics := os.Getenv("CSSMINIFIER_GOOGLE_ANALYTICS")
+
+	// load up all templates
+	tmpl, err := template.New("").ParseGlob("./templates/*.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// the mux
+	m := mux.New()
+
+	// do some static routes before doing logging
+	m.All("/s", fileServer("static"))
+	m.Get("/favicon.ico", serveFile("./static/favicon.ico"))
+	m.Get("/robots.txt", serveFile("./static/robots.txt"))
+
+	m.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		data := struct {
+			BaseUrl         string
+			GoogleAnalytics string
+		}{
+			baseUrl,
+			googleAnalytics,
+		}
+		render(w, tmpl, "index.html", data)
+	})
+
+	// finally, check all routing was added correctly
+	check(m.Err)
+
+	// server
+	log.Printf("Starting server, listening on port %s\n", port)
+	errServer := http.ListenAndServe(":"+port, m)
+	check(errServer)
 }
